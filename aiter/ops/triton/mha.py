35b1f6940f2587b51e9bb6a0baa3bddb6c9f020d
Kunal Tyagi <tyagi.kunal@live.com>
2025-07-11 14:29:58 +0000

    exp2

--- aiter/ops/triton/mha.py
+++ aiter/ops/triton/mha.py
@@ -279,10 +279,10 @@ def _attn_fwd_inner(
             qk += alibi_block / SM_SCALE
         # get max scores so far
         m_ij = tl.maximum(m_i, tl.max(qk, 1))
-        m_ij_scaled = m_ij * SM_SCALE * RCP_LN2
+        m_ij_scaled = m_ij * SM_SCALE
 
         # scale and subtract max
-        q_shifted = qk * SM_SCALE * RCP_LN2 - m_ij_scaled[:, None]
+        q_shifted = qk * SM_SCALE - m_ij_scaled[:, None]
 
         # Compute scaled QK and softmax probabilities
         p = tl.math.exp2(q_shifted)
@@ -309,7 +309,7 @@ def _attn_fwd_inner(
         # -- update output accumulator --
         # alpha is an adjustment factor for acc and li as we loop and find new maxes
         # store the diff in maxes to adjust acc and li as we discover new maxes
-        m_diff_scaled = m_i * SM_SCALE * RCP_LN2 - m_ij_scaled
+        m_diff_scaled = m_i * SM_SCALE - m_ij_scaled
         alpha = tl.math.exp2(m_diff_scaled)
         acc = acc * alpha[:, None]
         v = _load_fn(v_ptrs, k_offs_n, k_offs_k, seqlen_k, BLOCK_DMODEL)
